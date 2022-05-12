a5e60c568b84ae5129ca8ae3f5b58d2bd6174897
Felipe José <lipejose@icloud.com>
2022-05-12 08:53:55 -0300

    fix: absolute paths

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -12,7 +12,8 @@ export default {
     '@test/(.*)': '<rootDir>/test/$1',
     '@presentation/(.*)': '<rootDir>/src/presentation/$1',
     '@app/(.*)': '<rootDir>/src/application/$1',  
-    '@domain/(.*)': '<rootDir>/src/domain/$1'
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1'
   },
   collectCoverageFrom: [
     'src/**/*.ts',
